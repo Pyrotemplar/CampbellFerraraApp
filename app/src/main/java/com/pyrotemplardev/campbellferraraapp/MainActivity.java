c4c8bb77bce5b2e4924f1c8fc66c75be0359559c
Pyrotemplar <mlm09k@gmail.com>
2017-01-16 16:40:51 -0500

    Landing screen ui complated

--- app/src/main/java/com/pyrotemplardev/campbellferraraapp/MainActivity.java
+++ app/src/main/java/com/pyrotemplardev/campbellferraraapp/MainActivity.java
@@ -2,7 +2,7 @@ package com.pyrotemplardev.campbellferraraapp;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Window;
+
 
 public class MainActivity extends Activity {
 
@@ -10,7 +10,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //Remove title bar
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+      //  requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
     }
 }
